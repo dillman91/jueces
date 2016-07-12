9f44ffdeeab1a6accb359831d359e8b57bea3814
dillman91 <dillmansh@gmail.com>
2016-07-12 11:56:10 -0500

    Formulario de busqueda Solucionado

--- moduloPublico/web/buscador.php
+++ moduloPublico/web/buscador.php
@@ -43,31 +43,29 @@
 	<!-- /Banner -->
 
 	<!-- Main -->
-<div class="container">
-	<div class="row" style="height:100px">
-    	<div class="col-md-12 text-center" style="height:100px;"><font size="+3" style="font-weight:600;">BUSCADOR</font></div>
-    </div>
-<form action="buscador.php" method="post" name="buscar" style="background:#FFFFFF;">
-     <div class="row col-md-12">
+<section style="background:#FFF" class="container table-responsive">
+<form action="buscador.php" method="post" name="buscar" style="background:#;">
+   	<div class="col-md-12 text-center label-warning" style="height:100px;"><h2><strong>BUSCADOR</strong></h2></div>
+	<div class="row col-md-12">
        <div class="col-md-6 text-right"><h4>NOMBRE</h4></div><div class="col-md-3"><input type="text" name="nombre" class="form-control"/></div>
     </div>
-    <div class="row col-md-12 frente">
+    <div class="row col-md-12">
 		<div class="col-md-6 text-right"><h4>SERIE</h4></div><div class="col-md-3"><input type="text" name="serie" class="form-control"/></div>
     </div>
-    <div class="row col-md-12 frente">
+    <div class="row col-md-12">
 		<div class="col-md-6 text-right"><h4>CAUSA</h4></div><div class="col-md-3"><input type="text" name="causa" class="form-control" /></div>
     </div>
-    <div class="row col-md-12 frente">
+    <div class="row col-md-12">
 		<div class="col-md-6 text-right"><h4>PROCEDENCIA</h4></div><div class="col-md-3"><input type="text" name="procedencia" class="form-control"/></div>
     </div>
-    <div class="row col-md-12 frente">
+    <div class="row col-md-12">
 		<div class="col-md-6 text-right" style="height:50px;"><h4>PROVINCIA</h4></div><div class="col-md-3"><input type="text" name="provincia" class="form-control"/></div>
     </div>
-    <div class="row col-md-12 frente">
-		<div class="col-md-12 text-center" style="height:200px;"><input type="submit" name="btnBusca" value="Buscar" class="btn btn-warning btn-lg"/></div>
+    <div class="row col-md-12">
+		<div class="col-md-12 text-center" style="height:150px;"><input type="submit" name="btnBusca" value="Buscar" class="btn btn-warning btn-lg"/></div>
     </div>
 </form>
-</div>
+
 
 <?php
 	if(isset($_REQUEST['btnBusca'])){
@@ -80,8 +78,8 @@
 		$provincia	=$_REQUEST['provincia'];
 	$per = $a->buscarGeneral($nombre,$serie,$causa,$procedencia,$provincia);
 ?>
-<table class="table table-hover text-center" bgcolor="#FFF">
-<thead bgcolor="#3BA5CB" style="font-weight:600;color:#FFF">
+<table class="table table-hover text-center">
+<thead bgcolor="#3BA5CB" style="font-weight:600;">
     	<td>ID</td>
         <td>NOMBRES</td>
         <td>INICIO</td>
@@ -114,74 +112,9 @@
 <?php }
 	}
 ?>
-
-		
 </table>
-
-	<!-- Footer -->
-		<div id="footer">
-			<div class="container">
-				<div class="row">
-					<div class="3u">
-						<section>
-							<h2>Maecenas lectus</h2>
-							<div class="balloon">
-								<blockquote>&ldquo;&nbsp;&nbsp;Donec leo, vivamus ullamcorper fermentum nibh in augue pulvinar ullamcorper metus praesent a lacus at urna congue ullamcorper  rutrum.&nbsp;&nbsp;&rdquo;<br>
-									<br>
-									<strong>&ndash;&nbsp;&nbsp;John Smith</strong></blockquote>
-							</div>
-							<div class="ballon-bgbtm">&nbsp;</div>
-						</section>
-					</div>
-					<div class="3u">
-						<section>
-							<h2>Donec dictum</h2>
-							<ul class="default">
-								<li>
-									<h3>Mauris vulputate dolor sit amet</h3>
-									<p><a href="#">Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum.</a></p>
-								</li>
-								<li>
-									<h3>Fusce ultrices fringilla metus</h3>
-									<p><a href="#">Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum.</a></p>
-								</li>
-								<li>
-									<h3>Donec dictum metus in sapien</h3>
-									<p><a href="#">Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum.</a></p>
-								</li>
-							</ul>
-						</section>
-					</div>
-					<div class="3u">
-						<section>
-							<h2>Nulla leifend</h2>
-							<p>Donec placerat odio vel elit. Nullam ante orci, pellentesque eget, tempus quis, ultrices in, est. Curabitur sit amet nulla. Nam in massa. Sed vel tellus. Curabitur sem urna, consequat.</p>
-							<ul class="style5">
-								<li><a href="#"><img src="images/pics07.jpg" alt=""></a></li>
-								<li><a href="#"><img src="images/pics08.jpg" alt=""></a></li>
-								<li><a href="#"><img src="images/pics09.jpg" alt=""></a></li>
-								<li><a href="#"><img src="images/pics10.jpg" alt=""></a></li>
-								<li><a href="#"><img src="images/pics11.jpg" alt=""></a></li>
-								<li><a href="#"><img src="images/pics12.jpg" alt=""></a></li>
-							</ul>
-							<a href="#" class="button">More Collections</a>
-						</section>
-					</div>
-					<div class="3u">
-						<section>
-							<h2>Luctus eleifend</h2>
-							<p><strong>Aliquam erat volutpat. Pellentesque tristique ante ut risus. </strong></p>
-							<p>Quisque dictum. Integer nisl risus, sagittis convallis, rutrum id, elementum congue, nibh. Suspendisse dictum porta lectus. Donec placerat odio vel elit.</p>
-							<p>Donec placerat odio vel elit. Nullam ante orci, pellentesque eget, tempus quis, ultrices in, est. Curabitur sit amet nulla. Nam in massa. Sed vel tellus. Curabitur sem urna, consequat.</p>
-							<a href="#" class="button">More Collections</a>
-						</section>
-					</div>
-				</div>
-			</div>
-		</div>
-	<!-- /Footer -->
-
-	<!-- Copyright -->
+</section>
+<!-- Copyright -->
 		<div id="copyright" class="container">
 			Design: <a href="http://templated.co">TEMPLATED</a> Images: <a href="http://unsplash.com">Unsplash</a> (<a href="http://unsplash.com/cc0">CC0</a>)
 		</div>
